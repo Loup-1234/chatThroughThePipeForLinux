68f6a91dcae3359364bd029ac7da92150be1f467
Loup_1234 <humeaualain5@gmail.com>
2026-01-01 18:39:17 +0100

    Nothing to say

--- cmake-build-debug/CMakeFiles/4.2.1/CMakeSystem.cmake
+++ cmake-build-debug/CMakeFiles/4.2.1/CMakeSystem.cmake
@@ -3,7 +3,7 @@ set(CMAKE_HOST_SYSTEM_NAME "Linux")
 set(CMAKE_HOST_SYSTEM_VERSION "6.18.2-2-cachyos")
 set(CMAKE_HOST_SYSTEM_PROCESSOR "x86_64")
 
-
+include("/home/loup/.vcpkg-clion/vcpkg/scripts/buildsystems/vcpkg.cmake")
 
 set(CMAKE_SYSTEM "Linux-6.18.2-2-cachyos")
 set(CMAKE_SYSTEM_NAME "Linux")
